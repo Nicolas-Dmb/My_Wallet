36d292f817f63649cb42ad619730632c322587a8
Nicolas_dmb <gyfeix@gmail.com>
2024-07-15 23:23:46 +0200

    models User

--- API/API/settings.py
+++ API/API/settings.py
@@ -129,12 +129,14 @@ STATIC_URL = "static/"
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 
+
+AUTH_USER_MODEL = 'User.User'
+
 REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': (
         'rest_framework_simplejwt.authentication.JWTAuthentication',
     )
 }
-
 
 SIMPLE_JWT = {
     "ACCESS_TOKEN_LIFETIME": timedelta(minutes=5),
--- API/User/models.py
+++ API/User/models.py
@@ -1,3 +1,57 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
+import uuid
+from datetime import datetime
+from django.conf import settings
 
-# Create your models here.
+'''
+- Tester de créer un user avec et sans les blank True
+- Tester de créer un user avec deux fois la même Username/Email/Phone
+- Configurer les settings en faisant une erreur dans CurrencyList
+- Configurer les settings en faisant une erreur dans Colors 
+- Configurer normalement le setting 
+- Se connecter 
+- refresh 
+'''
+
+class User(AbstractUser):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    phone = models.CharField(max_length=20, unique=True, blank=False)
+    country = models.CharField(max_length=100, blank=True)
+    job = models.CharField(max_length=100, blank=True)
+    income = models.CharField(max_length=100, blank=True)
+    birthday = models.DateField(blank=True, null=True)
+    date_joined = models.DateTimeField(default=datetime.now)
+    email_verif = models.BooleanField(default=False)
+
+    class Meta:
+        ordering = ['username']
+
+    def __str__(self):
+        return self.username
+    
+class Setting(models.Model):
+    class CurrencyList(models.TextChoices): 
+        Euro = 'Euro', 
+        Dollar = 'Dollar',
+    
+    class Colors(models.TextChoices): 
+        Gray = 'Gray', 
+        Red = 'Red', 
+        Pink = 'Pink', 
+        Purple = 'Purple', 
+        Blue = 'Blue', 
+        Green = 'Green', 
+        Brown = 'Brown', 
+        Yellow = 'Yellow', 
+    
+    currency = models.CharField(max_length=10, default=CurrencyList.Euro, choices=CurrencyList.choices, blank=False)
+    nightMode = models.BooleanField(default=True, blank=False)
+    color = models.CharField(max_length=20, default=Colors.Red, choices=Colors.choices, blank=False)
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.user.username
+    
+
+
