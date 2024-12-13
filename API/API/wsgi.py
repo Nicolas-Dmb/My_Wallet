a5c08673c9ee45848bb76df9f2af820a137d27f8
Nicolas_dmb <gyfeix@gmail.com>
2024-12-13 15:57:53 +0100

    modif settings.py

--- API/API/wsgi.py
+++ API/API/wsgi.py
@@ -15,4 +15,4 @@ from django.core.wsgi import get_wsgi_application
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "API.API.settings")
 
 application = get_wsgi_application()
-app = application
+#app = application
